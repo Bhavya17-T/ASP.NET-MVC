5cfe98a27fba422d16222190e693a36efd10d908
Bhavya17-T <109224832+Bhavya17-T@users.noreply.github.com>
2022-08-22 10:00:14 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BootStrap.Models;
+
+namespace BootStrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<FootballBootStrap> qry = new List<FootballBootStrap>();
+            using (AssignmentEntities1 dc = new AssignmentEntities1())
+            {
+                qry = (from s in dc.FootBallLeagues
+                       where s.Status == "Win"
+
+                       select new FootballBootStrap
+                       {
+                           MatchID = s.MatchID,
+                           TeamName1 = s.TeamName1,
+                           TeamName2 = s.TeamName2,
+                           Status = s.Status,
+                           WinningTeam = s.WinningTeam,
+                           Points = s.Points,
+
+
+                       }).ToList();
+
+            }
+            return View(qry);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
